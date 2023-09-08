366402c679728c522cf05862b3347a7a21748d85
will_niutao <niutao2@huawei.com>
2023-09-08 14:55:48 +0800

    fix: fix octomap compile error
    Build error:
    ERROR 0002: file '/opt/ros/humble/lib64/liboctomap.so.1.9.8' contains an invalid rpath '/opt/ros/humble/lib'
    
    The CMakeLists.txt of octomap will change rpath of liboctomap.so to /opt/ros/humble/lib, but it isntall
    liboctomap.so to /opt/ros/humble/lib64
    
    All the ROS package are install in /opt/ros/xxx, so add CMAKE_INSTALL_LIBDIR to force install dir to
    /opt/ros/xx/lib

--- gen-pkg-spec.sh
+++ gen-pkg-spec.sh
@@ -200,8 +200,8 @@ modify_spec()
 
         debug_log "gen changelog"
         maintainer=`grep maintainer: ${ROS_DEPS_BASE}/$pkg-PackageXml | awk -F"maintainer:" '{print $2}'`
-        #changetime=`date +"%a %b %d %Y"`
-        changetime="Thu May 04 2023"
+        changetime=`date +"%a %b %d %Y"`
+        #changetime="Thu May 04 2023"
         changelog="$changetime $maintainer - $base_version-$release_version"
         sed -i "s#ROS_PACKAGE_CHANGELOG#$changelog#g" $spec
 
